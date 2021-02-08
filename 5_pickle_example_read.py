6914fd8ec64e84d54f1f28e611d2e8db9b0a7df0
Chris Keller <ckeller191@gmail.com>
2021-02-08 15:39:08 -0600

    class work 2/8/21

---
+++ 5_pickle_example_read.py
@@ -0,0 +1,22 @@
+import pickle
+
+infile = open("names_pickle_file_write.dat", "rb")
+
+names = pickle.load(infile)
+
+print(type(names))
+
+print(names)
+
+name = input("Add a name to the list: ")
+names.append(name)
+
+print(names)
+
+infile.close()
+
+outfile = open("names_pickle_file_write.dat", "wb")
+
+pickle.dump(names, outfile)
+
+outfile.close()
